56fa0d50508d54894c7044dc63ecc9423f7f2730
JMC <jmch0616@gmail.com>
2021-07-08 16:37:24 +0900

    update Firebase & join email

--- app/src/main/java/com/example/clone_instagram/LoginActivity.kt
+++ app/src/main/java/com/example/clone_instagram/LoginActivity.kt
@@ -1,11 +1,55 @@
 package com.example.clone_instagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        email_login_button.setOnClickListener {
+            signinAndSignup()
+        }
     }
+    // 아이디 생성 함수
+    fun signinAndSignup() {
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+            ?.addOnCompleteListener {
+                task ->
+                if(task.isSuccessful){
+                    moveMainPage(task.result?.user)
+                } else if(!task.exception?.message.isNullOrEmpty()) {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                } else {
+                    signinEmail()
+                }
+            }
+    }
+    // 로그인 함수
+    fun signinEmail() {
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    moveMainPage(task.result?.user)
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+    // 로그인 성공시 다음화면 함수
+    fun moveMainPage(user: FirebaseUser?) {
+        if(user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+    }
+
 }
