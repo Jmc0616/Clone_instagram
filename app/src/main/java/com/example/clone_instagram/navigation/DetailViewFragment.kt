950ea5612f7a089afd8847f7f1d5ee53cec7aa42
JMC <jmch0616@gmail.com>
2021-07-15 23:58:54 +0900

    update detailView

--- app/src/main/java/com/example/clone_instagram/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/clone_instagram/navigation/DetailViewFragment.kt
@@ -5,15 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.clone_instagram.R
+import com.example.clone_instagram.navigation.model.ContentDTO
+import com.google.firebase.firestore.FirebaseFirestore
 
 class DetailViewFragment : Fragment() {
+    var firestore : FirebaseFirestore? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail,container,false)
+        firestore = FirebaseFirestore.getInstance()
+
         return view
+    }
+    inner class DetailViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+        var contentUidList : ArrayList<String> = arrayListOf()
+
+//        init {
+//            firestore?.collection("images")?.orderBy("timstamp")?.addSnapshotListener()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
     }
 }
